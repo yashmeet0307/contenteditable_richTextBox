e7f37a504551b75690b7b61e66d2ae5f427a140b
yashmeet0307 <yashmeet0307@yahoo.co.in>
2016-05-28 01:54:51 +0530

    code committed

---
+++ config.js
@@ -0,0 +1,6 @@
+var emojiConfig = {};
+emojiConfig.path = 'packs/basic';
+emojiConfig.icons = {
+	':)'  : 'smile.png',
+	':D'  : 'laughing.png'
+};
---
+++ emojiarea.js
@@ -0,0 +1,108 @@
+(function(config,window){
+			emojiArea = {
+				range : '',
+				selection : '',
+				map : [],
+				childCount : document.getElementById('userInput').children.length,
+				init : function(){
+            		this.initiateKeyUp();
+            		this.initiatePress();
+        		},
+				createIcon : function(emoji) {
+					var filename = config.icons[emoji];
+					var path = config.path || '';
+					if (path.length && path.charAt(path.length - 1) !== '/') {
+						path += '/';
+					}
+					return '<span><img src="' + path + filename + '" alt=""></span>';
+				},
+				reEscape : function(s) {
+		        	return s.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+		    	},
+		    	setEndOfContenteditable : function(contentEditableElement){
+		    		var that = emojiArea;
+				    if(document.createRange){
+				        that.savedRange = document.createRange();
+				        that.savedRange.selectNodeContents(contentEditableElement);
+				        that.savedRange.collapse(false);
+				        that.selection = window.getSelection();
+				        that.selection.removeAllRanges();
+				        that.selection.addRange(that.savedRange);
+				    }
+				    else if(document.selection){ 
+				        that.range = document.body.createTextRange();
+				        that.range.moveToElementText(contentEditableElement);
+				        that.range.collapse(false);
+				        that.range.select();
+				    }
+				},
+				initiateKeyUp : function(){
+					document.getElementById('userInput').addEventListener("keyup",function(e) {
+						var that = emojiArea;
+				    	var self = this;
+				    	var that = emojiArea;
+				    	var html = this.innerHTML;
+				    	var previousHTML = html;
+				    	var emojis = config.icons;
+				    	if(e.keyCode !== 13){
+							for (var key in emojis) {
+								if (emojis.hasOwnProperty(key)) {
+									html = html.replace(new RegExp(that.reEscape(key), 'g'),that.createIcon(key));
+								}
+							}
+							if(previousHTML != html){
+								this.innerHTML = html;
+							}
+				    		var newChildCount  = this.children.length;
+				    		if(that.childCount != newChildCount){
+				    			that.setEndOfContenteditable(this.children[newChildCount-1]);
+				    			that.childCount = newChildCount;
+				    		}
+						}else if(e.keyCode == '13'){
+				    		var iDiv = document.createElement('div');
+							iDiv.id = 'block';
+							iDiv.className = 'com-o-s';
+							iDiv.innerHTML = html;
+							document.getElementById('append').appendChild(iDiv);
+							this.innerHTML = '';
+				    	}
+	
+		 		 	});
+				},
+				initiatePress : function(){
+					document.getElementById('userInput').addEventListener("keypress",function(e) {
+						console.log('keypress');
+						var style,fontSize,image,height,width;
+						if(e.metaKey && (e.keyCode == '97' || e.keyCode == '65')){
+							document.execCommand('insertHTML', false);
+							e.preventDefault();
+							style = window.getComputedStyle(this, null).getPropertyValue('font-size');
+							fontSize = parseFloat(style); // now you have a proper float for the font size (yes, it can be a float, not just an integer)
+							this.style.fontSize = (fontSize + 1) + 'px';
+							if(this.getElementsByTagName('img').length > 0){
+								image = this.getElementsByTagName('img')[0];
+								height = parseFloat(window.getComputedStyle(image, null).getPropertyValue('height'));
+								width = parseFloat(window.getComputedStyle(image, null).getPropertyValue('width'));
+								image.style.height = (height + 1) + 'px';
+								image.style.width = (width + 1) + 'px';
+							}
+						}
+						else if(e.metaKey && (e.keyCode == '78' || e.keyCode == '110')){
+							document.execCommand('insertHTML', false);
+							e.preventDefault();
+							style = window.getComputedStyle(this, null).getPropertyValue('font-size');
+							fontSize = parseFloat(style); // now you have a proper float for the font size (yes, it can be a float, not just an integer)
+							this.style.fontSize = (fontSize - 1) + 'px';
+							if(this.getElementsByTagName('img').length > 0){
+								image = this.getElementsByTagName('img')[0];
+								height = parseFloat(window.getComputedStyle(image, null).getPropertyValue('height'));
+								width = parseFloat(window.getComputedStyle(image, null).getPropertyValue('width'));
+								image.style.height = (height - 1) + 'px';
+								image.style.width = (width - 1) + 'px';
+							}
+						}
+					});
+				}
+			};
+		emojiArea.init();
+		})(emojiConfig,window);
